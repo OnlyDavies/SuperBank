8cad16f439c958407bd023f2894c3710c1d2c306
onlydavies <onlydavies93@gmail.com>
2021-05-14 13:26:13 +0200

    First

---
+++ SuperBank/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("David", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+            account.MakeWithdrawal(120,DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
---
+++ SuperBank/Transaction.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text;
+
+namespace SuperBank
+{
+    public class Transaction
+    {
+        public decimal Amount { get; }
+
+        public DateTime Date { get; }
+        
+        public string Notes { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+
+    }
+
+}
